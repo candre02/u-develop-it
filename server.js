cf4102d0e0378d0ef1b3a4a73910dbdaded7f66a
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-12-01 02:43:10 -0600

    created a connection file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const mysql = require("mysql2");
+// import files
 const express = require('express');
 const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,16 +9,6 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection({
-  host: 'localhost',
-  // Your MySQL username,
-  user: 'root',
-  // Your MySQL password
-  password: '',
-  database: 'election'
-});
 
 // Get all candidates and their party affiliation
 app.get('/api/candidates', (req, res) => {
